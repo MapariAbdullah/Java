4991d20adbfd43fde31fe56a007f55ab24023858
eshabawaskar <68116857+eshabawaskar@users.noreply.github.com>
2020-10-02 19:43:11 +0530

    Update Boolean.java

--- Boolean.java
+++ Boolean.java
@@ -1,12 +1,12 @@
 public class Boolean{
 	
-	public static void main(String args[])
+	public static void main(String args[]) 
 	{
 		int x=6;
 		int y=-6;
 		System.out.println(""+x);
 		System.out.println(""+y);
-		System.out.println(""+(x>>2));
+		System.out.println(""+(x>>2));//output
 		System.out.println(""+(x<<1));
 	}
 }
